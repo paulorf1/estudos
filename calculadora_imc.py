6923fca2d98e78060efd10464988b0a66dec13cf
Paulo Ramos <paulorf88@gmail.com>
2025-07-08 18:02:48 -0300

    Create calculadora_imc.py
    Calculadora de imc em python.

---
+++ calculadora_imc.py
@@ -0,0 +1,19 @@
+nome = input('Digite seu nome: ')
+peso_str = input('Digite seu peso: ')
+peso = float(peso_str.replace(',', '.'))
+altura_str = input('Digite sua altura: ')
+altura = float(altura_str.replace(',', '.'))
+
+imc = peso / (altura ** 2)
+imc_arredondado = round(imc, 2)
+if imc_arredondado <= 18.5:
+    print('Seu IMC está abaixo do peso')
+elif imc_arredondado >= 18.5 and imc_arredondado <= 24.99:
+    print('Seu IMC está normal')
+elif imc_arredondado >= 25 and imc_arredondado <= 29.99:
+    print('Seu IMC está sobrepeso')
+else:
+    print('Seu IMC está Obeso')
+
+print(nome, 'Tem', altura, 'De altura')
+print('Pesa', peso, 'Quilos e seu imc é:', imc_arredondado)
